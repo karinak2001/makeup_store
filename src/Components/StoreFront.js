0dddaf6acf1524ddcaf498d15da4539b3bf63df0
michelle schneider <michesc@edu.aac.ac.il>
2025-01-03 23:57:52 +0200

    initial commit

--- src/Components/StoreFront.js
+++ src/Components/StoreFront.js
@@ -70,7 +70,7 @@ function StoreFront() {
                     <div className="main">
                         <div className="title">Glow Up</div>
                         <div className="cart-button">
-                            <button type="button" className="btn btn-light" onClick={() => navigate('/new')}>
+                            <button type="button" className="btn btn-light cart-button-style" onClick={() => navigate('/new')}>
                                 <i className="bi bi-cart-check-fill large-icon"></i></button>
                         </div>
                         <MakeupProductsTable items={makeupProducts}/>
